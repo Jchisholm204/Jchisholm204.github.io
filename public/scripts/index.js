9ad8d311f42515147368c5be3c75a468238767ba
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2022-12-16 23:07:21 -0500

    everything works as I like
    mobile, desktop resize, 404..

---
+++ public/scripts/index.js
@@ -0,0 +1,70 @@
+/**
+ * @author Jacob Chisholm
+ * 
+ */
+
+function windowResize(){
+    const platform = navigator.userAgent;
+    let deviceType = "desktop";
+
+    if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(platform)) {
+        deviceType = "tablet";
+    }
+    else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(platform)) {
+        deviceType = "mobile";
+    }
+
+    const sidebar = document.getElementById("sidebar");
+    const content = document.getElementById("page-content");
+    const contactHeader = document.getElementById("contact-header");
+    const header = document.getElementById("sidebar-header");
+    const footer = document.getElementById("sidebar-footer");
+    const authAbout = document.getElementById("auth-about");
+    //const article = document.getElementsByName("article");
+
+    /*if(deviceType === "mobile"){
+        contactHeader.style.visibility = 'hidden';
+        authAbout.style.textAlign = 'center';
+
+        sidebar.style.position = 'static';
+        sidebar.style.justifyContent = 'space-between';
+        sidebar.style.width = '100vw';
+        sidebar.style.minWidth = '100vw';
+        sidebar.style.height = 'calc(285px - 8vw)';
+        sidebar.style.flexDirection = 'column';
+
+        content.style.margin = '0px';
+        content.style.width = "100%";
+    }*/
+    if(window.innerWidth < 900 || deviceType === "mobile"){
+        contactHeader.style.visibility = 'hidden';
+        authAbout.style.textAlign = 'center';
+
+        sidebar.style.position = 'static';
+        sidebar.style.justifyContent = 'space-between';
+        sidebar.style.width = '100%';
+        //sidebar.style.minWidth = '600px';
+        sidebar.style.height = 'calc(285px - 8vw)';
+        sidebar.style.flexDirection = 'column';
+
+        content.style.marginLeft = '5px';
+    }
+    else{
+        contactHeader.style.visibility = 'visible';
+
+        sidebar.style.position = 'fixed';
+        sidebar.style.justifyContent = 'space-between';
+        sidebar.style.width = '240px';
+        sidebar.style.minWidth = '0px';
+        sidebar.style.height = '100%';
+        sidebar.style.flexDirection = 'column';
+
+        content.style.marginLeft = '245px';
+    }
+}
+
+window.onload = () => {
+    windowResize();
+}
+
+window.addEventListener("resize", windowResize, true)
--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
+const device = require("express-device");
 
-const hostname = '127.0.0.1'
-const port = 1337;
+const hostname = '192.168.86.2'
+const port = 80;
 
 router.use(express.static(__dirname + '/public'));
 router.use(express.static(__dirname + '/public/styles'));
@@ -14,13 +15,21 @@ router.get('/', (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
 
-router.use((req, res) => {
-    res.sendFile(__dirname + "/public/404.html");
+router.get('/device', (req, res) => {
+    res.send(req.device.type);
 });
+
+app.use(device.capture());
+
+router.use((req, res) => {
+    res.sendFile(__dirname + "/public/pages/404.html");
+});
+
+app.use(express.static("public"));
 
 app.use('/', router);
 
-app.listen(port, hostname, () => {
+app.listen(port, () => {
     console.log(`Server running at ${hostname}:${port}`);
     console.log(__dirname);
 });
